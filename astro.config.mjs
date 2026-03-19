1157f286ae372754337d44c12e06f1271a2da9be
kanaftc <hani.lgd@gmail.com>
2026-03-19 06:22:47 +0100

    fix: base path restored + full rebuild with media

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://pebify.be',
+  site: 'https://kanaftc.github.io',
+  base: '/pebify-premium',
   integrations: [tailwind()],
   output: 'static',
   build: {
